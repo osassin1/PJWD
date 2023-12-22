bcd32f97bd419b179039c30d3e6edf1f8f77d223
Oliver Sassin <oliver.sassin@iu-study.org>
2023-12-22 12:16:39 -0500

    adding navigation component

--- FamilyShoppingList-client/src/app/app.component.spec.ts
+++ FamilyShoppingList-client/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'FamilyShoppingList-client' title`, () => {
+  it(`should have the 'FamilyShoppingList' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('FamilyShoppingList-client');
+    expect(app.title).toEqual('FamilyShoppingList');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, FamilyShoppingList-client');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, FamilyShoppingList');
   });
 });
